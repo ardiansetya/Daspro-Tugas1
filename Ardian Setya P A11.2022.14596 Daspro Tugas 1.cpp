1c74b1cafe7e028e185651bbf030e693ed903912
ardiansetya <113000649+ardiansetya@users.noreply.github.com>
2022-09-07 22:03:09 +0700

    Add files via upload

---
+++ Ardian Setya P A11.2022.14596 Daspro Tugas 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "------BIODATA------" << endl;
+    cout << endl;
+    cout << "Ardian Setya Pradana" << endl;
+    cout << "A11.2022.14596" << endl;
+    cout << "Pati, 04 Oktober 2003" << endl;
+    cout << "Jalan Syeh jangkung RT.7 RW.4" << endl;
+    cout << "asal sekolah SMA Negeri 1 Pati" << endl;
+    return 0;
+}
